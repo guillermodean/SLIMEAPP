a1fa726d4c1507b113519a4a92b07d635c65f3c6
Guillermoç <guillermodean@hotmail.com>
2021-04-01 11:39:29 +0200

    first commit angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
-const routes: Routes = [];
+//components
+import {TaskComponent} from './components/task/task.component'
+import {SigningComponent} from './components/signing/signing.component'
+import {SignupComponent} from './components/signup/signup.component'
+import {PrivateTasksComponent} from './components/private-tasks/private-tasks.component'
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'task',
+    pathMatch:'full'
+  },
+  {
+    path:'task',
+    component:TaskComponent
+  },
+  {
+    path:'signin',
+    component:SigningComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  },
+  {
+    path:'private',
+    component:PrivateTasksComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes),MatTabsModule],
+  exports: [RouterModule,MatTabsModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigningComponent } from './components/signing/signing.component';
+import { TaskComponent } from './components/task/task.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigningComponent,
+    TaskComponent,
+    PrivateTasksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
